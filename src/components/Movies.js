6066f6c365fa95c67bbb6843c3b720f7db5eecc5
Alec Hall <alecnhall@gmail.com>
2020-05-19 16:22:58 -0600

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,18 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie =>
+          <div key={movie.title}> 
+            <h1>{movie.title}</h1>
+            <h2> Run Time: {movie.time}</h2>
+            <ul>
+              {movie.genres.map(genre =>
+                <li key={genre}>{genre}</li>
+              )}
+            </ul>
+          </div>
+          )}
     </div>
   );
 };
