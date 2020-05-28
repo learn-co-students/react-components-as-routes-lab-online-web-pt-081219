debb7a36f59edd508b3ce152c11d66e5399df22a
Shaqqour <mshaqqourm@gmail.com>
2020-05-27 18:48:23 -0700

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,21 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {
+        directors.map(director => {
+          return (
+            <div key={director.name}>
+              <h3>Name: {director.name}</h3>
+              <p>Movies:
+                <ul>
+                  {director.movies.map(movie => <li key={movie}>{movie}</li>)}
+                </ul>
+              </p>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,22 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {
+        movies.map(movie => {
+          return (
+            <div key={movie.title}>
+              <h3>Name: {movie.title}</h3>
+              <p>Time: {movie.time}</p>
+              <p>Genres:
+                <ul>
+                  {movie.genres.map(genre => <li key={genre}>{genre}</li>)}
+                </ul>
+              </p>
+            </div>
+          )
+        })
+      }
     </div>
   );
 };
